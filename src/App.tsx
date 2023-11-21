3093059fb6a1ef489f25e35e33e53cc71d4cdadb
vepraaa <a.veprikovaa@yandex.ru>
2023-11-21 20:12:57 +0500

    bezoformleniya

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
+import { Input, Button, Text } from "@mantine/core";
 
 function App() {
-    return (
-        <>
-            <p>Проект ДПО</p>
-        </>
-    )
+  return (
+    <>
+    <Button>Назад</Button>
+    <Text>Имя</Text>
+    <Input placeholder="John"/>
+    <Text>Фамилия</Text>
+    <Input placeholder="Doe" />
+    <Text>Email</Text>
+    <Input placeholder="johndoe@mail.ru" />
+    <Text>Телефон</Text>
+    <Input placeholder="+1 111 111 1111" />
+    <Text>Адрес</Text>
+    <Input placeholder="123 Main Street" />
+    <Text>Индекс</Text>
+    <Input placeholder="123456" />
+    <Text>Город</Text>
+    <Input placeholder="New York City" />
+    <Text>Регион</Text>
+    <Input placeholder="New York" />
+    <Text>Страна</Text>
+    <Input placeholder="United States" />
+    <Button variant="filled">Отправить</Button>
+    </>
+  );
 }
-
-export default App
+  export default App
