f252a1256be38ae697f3b2fc0fd370ac5a5724e0
vepraaa <a.veprikovaa@yandex.ru>
2023-11-27 14:05:18 +0500

    rasstanovka

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,58 @@ import { Input, Button, Text } from "@mantine/core";
 function App() {
   return (
     <>
-    <Button>Назад</Button>
-    <Text>Имя</Text>
-    <Input placeholder="John"/>
-    <Text>Фамилия</Text>
-    <Input placeholder="Doe" />
-    <Text>Email</Text>
-    <Input placeholder="johndoe@mail.ru" />
-    <Text>Телефон</Text>
-    <Input placeholder="+1 111 111 1111" />
-    <Text>Адрес</Text>
-    <Input placeholder="123 Main Street" />
-    <Text>Индекс</Text>
-    <Input placeholder="123456" />
-    <Text>Город</Text>
-    <Input placeholder="New York City" />
-    <Text>Регион</Text>
-    <Input placeholder="New York" />
-    <Text>Страна</Text>
-    <Input placeholder="United States" />
-    <Button variant="filled">Отправить</Button>
+    <Button className={'nazad'}>Назад</Button>
+    <Text className={'nastroiki'}>Настройки</Text>
+    <Text className={'name'}>Имя</Text>
+    <Input 
+    className={'boxname'} 
+    placeholder="John"
+    />
+    <Text className={'fam'} >Фамилия</Text>
+    <Input 
+    className={'boxfam'} 
+    placeholder="Doe" 
+    />
+    <Text className={'email'} >Email</Text>
+    <Input 
+
+    className={'boxmail'} 
+    placeholder="johndoe@mail.ru" 
+    />
+    <Text className={'number'} >Телефон</Text>
+    <Input 
+    className={'boxnumber'} 
+    placeholder="+1 111 111 1111" 
+    />
+    <Text className={'address'} >Адрес</Text>
+    <Input 
+    className={'boxaddress'} 
+    placeholder="123 Main Street" 
+    />
+    <Text className={'index'} >Индекс</Text>
+    <Input 
+    className={'boxindex'} 
+    placeholder="123456" 
+    />
+    <Text className={'city'} >Город</Text>
+    <Input 
+    className={'boxcity'} 
+    placeholder="New York City" 
+    />
+    <Text className={'region'} >Регион</Text>
+    <Input 
+    className={'boxregion'} 
+    placeholder="New York" 
+    />
+    <Text className={'country'} >Страна</Text>
+    <Input 
+    className={'boxcountry'} 
+    placeholder="United States" 
+    />
+    <Button
+    variant="danger"
+    className={'send'}
+    >Сохранить</Button>
     </>
   );
 }
