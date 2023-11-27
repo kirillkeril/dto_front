4618908fb5178e91278d09758a2feee3e441e8e5
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-11-27 19:22:07 +0300

    fix profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,13 @@
 import './App.css'
-import { Input, Button, Text } from "@mantine/core";
+import '@mantine/core/styles.css';
+import Profile from "./pages/Profile.tsx";
 
 function App() {
-  return (
-    <>
-    <Button className={'nazad'}>Назад</Button>
-    <Text className={'nastroiki'}>Настройки</Text>
-    <Text className={'name'}>Имя</Text>
-    <Input 
-    className={'boxname'} 
-    placeholder="John"
-    />
-    <Text className={'fam'} >Фамилия</Text>
-    <Input 
-    className={'boxfam'} 
-    placeholder="Doe" 
-    />
-    <Text className={'email'} >Email</Text>
-    <Input 
-
-    className={'boxmail'} 
-    placeholder="johndoe@mail.ru" 
-    />
-    <Text className={'number'} >Телефон</Text>
-    <Input 
-    className={'boxnumber'} 
-    placeholder="+1 111 111 1111" 
-    />
-    <Text className={'address'} >Адрес</Text>
-    <Input 
-    className={'boxaddress'} 
-    placeholder="123 Main Street" 
-    />
-    <Text className={'index'} >Индекс</Text>
-    <Input 
-    className={'boxindex'} 
-    placeholder="123456" 
-    />
-    <Text className={'city'} >Город</Text>
-    <Input 
-    className={'boxcity'} 
-    placeholder="New York City" 
-    />
-    <Text className={'region'} >Регион</Text>
-    <Input 
-    className={'boxregion'} 
-    placeholder="New York" 
-    />
-    <Text className={'country'} >Страна</Text>
-    <Input 
-    className={'boxcountry'} 
-    placeholder="United States" 
-    />
-    <Button
-    variant="danger"
-    className={'send'}
-    >Сохранить</Button>
-    </>
-  );
+    return (
+        <div>
+            <Profile/>
+        </div>
+    );
 }
-  export default App
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { MantineProvider } from "@mantine/core";
+import {createTheme, MantineProvider} from "@mantine/core";
+
+const theme = createTheme({
+    fontFamily: "Open Sans, sans-serif"
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider defaultColorScheme="light">
+    <MantineProvider defaultColorScheme="light" theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
